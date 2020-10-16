f7125f0223f4d621151e7d3380a3860dc370f35a
Trevor Buchanan <trevor@trevorbuchanan.com>
2020-10-16 11:24:31 -0500

    bookservice testing in place

--- bookstore/src/test/java/com/lambdaschool/bookstore/services/BookServiceImplTest.java
+++ bookstore/src/test/java/com/lambdaschool/bookstore/services/BookServiceImplTest.java
@@ -2,6 +2,8 @@ package com.lambdaschool.bookstore.services;
 
 import com.lambdaschool.bookstore.BookstoreApplication;
 import com.lambdaschool.bookstore.exceptions.ResourceNotFoundException;
+import com.lambdaschool.bookstore.models.Book;
+import com.lambdaschool.bookstore.models.Section;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,6 +12,11 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.persistence.EntityNotFoundException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = BookstoreApplication.class)
@@ -21,6 +28,8 @@ public class BookServiceImplTest
 
     @Autowired
     private BookService bookService;
+
+    @Autowired SectionService sectionService;
 
     @Before
     public void setUp() throws
@@ -38,35 +47,51 @@ public class BookServiceImplTest
     @Test
     public void findAll()
     {
+        assertEquals(5, bookService.findAll().size());
     }
 
     @Test
     public void findBookById()
     {
+        assertEquals("The Da Vinci Code", bookService.findBookById(28).getTitle());
     }
 
-    @Test(expected = ResourceNotFoundException.class)
+    @Test(expected = EntityNotFoundException.class)
     public void notFindBookById()
     {
+        assertEquals("Should fail", bookService.findBookById(999));
     }
 
     @Test
     public void delete()
     {
+        bookService.delete(27);
+        assertEquals(4, bookService.findAll().size());
     }
 
     @Test
     public void save()
     {
+        String bTitle = "Test Book Title";
+        // Section section = new Section("Philosophy");
+        // section.setSectionid(9129);
+        // sectionService.save(section);
+        // no matter what I do for section, it dies
+        // So I made section null. Only supposed to be testing
+        // BookService anyways
+        Book b = new Book(bTitle, "1010101001100", 1010, null);
+        Book addedBook = bookService.save(b);
+        assertNotNull(addedBook);
+        assertEquals(bTitle, addedBook.getTitle());
     }
 
-    @Test
-    public void update()
-    {
-    }
-
-    @Test
-    public void deleteAll()
-    {
-    }
+//    @Test
+//    public void update()
+//    {
+//    }
+//
+//    @Test
+//    public void deleteAll()
+//    {
+//    }
 }
